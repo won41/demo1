b1edadfc7fcaaedb584d719ae48f54170810e740
won41 <tieh96@naver.com>
2024-10-10 10:07:54 +0900

    mybatis xml 설정

--- src/main/java/com/pgm/springdemo/mapper/TimeMapper.java
+++ src/main/java/com/pgm/springdemo/mapper/TimeMapper.java
@@ -3,9 +3,9 @@ package com.pgm.springdemo.mapper;
 import org.apache.ibatis.annotations.Select;
 
 public interface TimeMapper {
-    @Select("select now()")
+    //@Select("select now()")
     String getTime();
 
-    @Select("select count(*) from test_tb1")
+    //@Select("select count(*) from test_tb1")
     int getCount();
 }
